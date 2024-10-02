48f9aaa3000fcb28c68626b6ac65c4980018885c
bssadiya <156563575+bssadiya@users.noreply.github.com>
2024-10-02 22:23:54 +0530

    index.js

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// See https://github.com/dialogflow/dialogflow-fulfillment-nodejs
+// for Dialogflow fulfillment library docs, samples, and to report issues
 'use strict';
  
 const functions = require('firebase-functions');
@@ -22,7 +24,51 @@ function insurancetype(){
 function termlifeinsurance(){
   const termlifeinsurance = agent.parameters.termlifeinsurance;
 }
+function healthinsurance(){
+  const healthinsurance = agent.parameters.healthinsurance;
   
+}
+function investmentinsurance(){
+  const investmentsinsurance = agent.parameters.investmentsinsurance;
+}
+function otherplans(){
+  const otherplans = agent.parameters.otherplans;
+}
+function businessinsurance(){
+  const businessinsurance = agent.parameters.businessinsurance;
+}
+  
+ function savehandler(agent)
+  {
+    const {
+      name,age,phone,email,typeofinsurance,yearlyincome,investmentinsurance
+    } =agent.parameters;
+    const data=[{
+      Name: name,
+      Age: age,
+      Phone:phone,
+      Email: email,
+      Type_of_Insurance: typeofinsurance,
+      Yearly_Income: yearlyincome
+    }];
+    axios.post('https://sheetdb.io/api/v1/i90uxq2pvtapg',data);
+    agent.end("");
+  }
+  
+  // Run the proper function handler based on the matched Dialogflow intent name
+  let intentMap = new Map();
+  intentMap.set('Spreadsheetdemo', spreadsheet);
+  intentMap.set('Insurancetype',insurancetype);
+  intentMap.set('investmentinsurance',investmentinsurance);
+  intentMap.set('businessinsurance',businessinsurance);
+  intentMap.set('otherplans',otherplans);
+  intentMap.set('healthinsurance',healthinsurance);
+  //intentMap.set('Default Fallback Intent', fallback);
+  intentMap.set('Chatbot', savehandler);
+  
+
+  agent.handleRequest(intentMap);
+});
  function savehandler(agent)
   {
     const {
